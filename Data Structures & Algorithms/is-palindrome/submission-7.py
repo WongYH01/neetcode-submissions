b9338c32a5f919d335caa78d2258ffbdb2d562d0
WongYH01 <65993170+WongYH01@users.noreply.github.com>
2026-09-01 12:48:28 +0800

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Set left to start, right to length
+        # while the right is bigger than the left
+            # check if the curr elem is not an alphanum
+                # move
+            # check if the now elem is not the same
+                # false
+            # up L, down R
+        new_s = ""
+        for char in s:
+            if char.isalnum():
+                new_s += char.lower()
+
+        L, R = 0, len(new_s)-1
+        while R>L:
+            # if not s[L].isalnum():
+            #     L += 1
+            # if not s[R].isalnum():
+            #     R -= 1
+
+            if new_s[L] != new_s[R]:
+                return False
+            L += 1
+            R -= 1
+        return True
+
+        
+        
